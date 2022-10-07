6492dd65bbd80d47dea253451954a7f0828e9bee
Stefano <stefano.dafarra@gmail.com>
2022-10-07 16:15:06 +0200

    First version working with image being streamed to port.

--- src/src/Viewer.cpp
+++ src/src/Viewer.cpp
@@ -18,15 +18,155 @@
 #include <memory>
 #include <string>
 
+#include <vtkWindowToImageFilter.h>
+#include <vtkNew.h>
+
+#include <functional>
+#include <cstring>
+#include <thread>
+#include <csignal>
+#include <yarp/os/LogStream.h>
+#include <yarp/sig/Image.h>
+#include <yarp/os/BufferedPort.h>
+
 using namespace RobotsIO::Camera;
 using namespace RobotsViz;
+
+std::function<void ()> customHandlerLambda;
+
+void my_handler(int sig)
+{
+    static int ct = 0;
+
+    if (sig == SIGABRT)
+    {
+        yInfo() << "Aborted.";
+        if (ct > 3) //to avoid that std::abort is called again
+        {
+            return;
+        }
+    }
+
+    ct++;
+    if (ct > 3) {
+        yInfo() <<  "Aborting (calling abort())...";
+        std::abort();
+    }
+    yInfo() << "[try " << ct << " of 3] Trying to shut down.";
+
+    customHandlerLambda();
+}
+
+#ifdef WIN32
+
+#include <windows.h>
+
+BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
+{
+    switch (fdwCtrlType) {
+        // Handle the CTRL-C signal.
+    case CTRL_C_EVENT:
+    case CTRL_CLOSE_EVENT:
+    case CTRL_SHUTDOWN_EVENT:
+        my_handler(0);
+        return TRUE;
+
+    // Handle all other events
+    default:
+        return FALSE;
+    }
+}
+#endif
+
+void handleSignals(std::function<void ()> customHandler)
+{
+#ifdef WIN32
+    SetConsoleCtrlHandler(CtrlHandler, TRUE);
+#else
+    struct sigaction action;
+    memset(&action, 0, sizeof(action));
+    action.sa_handler = &my_handler;
+    sigaction(SIGINT, &action, NULL);
+    sigaction(SIGTERM, &action, NULL);
+    sigaction(SIGABRT, &action, NULL);
+#endif
+    customHandlerLambda = customHandler;
+}
+
+void vtkImageDataToYarpimage(vtkImageData* imageData, yarp::sig::FlexImage& image)
+{
+    if (!imageData)
+    {
+        return;
+    }
+
+    /// \todo retrieve just the UpdateExtent
+    int width = imageData->GetDimensions()[0];
+    int height = imageData->GetDimensions()[1];
+
+    bool useRGBA = false;
+    if (imageData->GetNumberOfScalarComponents() == 3)
+    {
+        image.setPixelCode(VOCAB_PIXEL_RGB);
+    }
+    else if (imageData->GetNumberOfScalarComponents() > 3)
+    {
+        image.setPixelCode(VOCAB_PIXEL_RGBA);
+        useRGBA = true;
+    }
+    else
+    {
+        return;
+    }
+
+    image.resize(width, height);
+
+    unsigned char* colorsPtr =
+            reinterpret_cast<unsigned char*>(imageData->GetScalarPointer());
+
+    // Loop over the vtkImageData contents.
+    for (int row = height - 1; row >= 0; row--)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            if (useRGBA)
+            {
+                yarp::sig::PixelRgba& pixelYarp = *(reinterpret_cast<yarp::sig::PixelRgba*>(
+                                                        image.getPixelAddress(col, row)));
+
+                //iDynTree specifies the pixels in [0.0, 1.0], yarp between in [0, 255]
+                pixelYarp.r = colorsPtr[0];
+                pixelYarp.g = colorsPtr[1];
+                pixelYarp.b = colorsPtr[2];
+                pixelYarp.a = colorsPtr[3];
+            }
+            else
+            {
+                yarp::sig::PixelRgb& pixelYarp = *(reinterpret_cast<yarp::sig::PixelRgb*>(
+                                                       image.getPixelAddress(col, row)));
+
+                //iDynTree specifies the pixels in [0.0, 1.0], yarp between in [0, 255]
+                pixelYarp.r = colorsPtr[0];
+                pixelYarp.g = colorsPtr[1];
+                pixelYarp.b = colorsPtr[2];
+            }
+            // Swap the vtkImageData RGB values with an equivalent QColor
+            colorsPtr += imageData->GetNumberOfScalarComponents();
+        }
+    }
+}
 
 
 int main(int argc, char** argv)
 {
-    if ((argc < 10) || ((std::string(argv[9]) == "true") && (argc != 11)))
+    yarp::os::Network yarp; //to initialize the network
+
+    bool closing = false;
+    handleSignals([&](){closing = true;});
+
+    if (argc < 7)
     {
-        std::cout << "Synopsis: robmo-icub-viz <blocking> <robot_name> <hand_laterality> <use_fingers> <use_analogs> <hand_fk> <hand_aruco> <hand_debug> <point_cloud> [<camera>]" << std::endl;
+        std::cout << "Synopsis: robmo-icub-viz <blocking> <robot_name> <hand_laterality> <use_fingers> <use_analogs> <hand_fk>" << std::endl;
         std::cout << "          Camera name <camera> is required only if <point_cloud> = true." << std::endl;
         return EXIT_FAILURE;
     }
@@ -34,9 +174,6 @@ int main(int argc, char** argv)
     bool use_fingers = false;
     bool use_analogs = false;
     bool show_hand_fk = false;
-    bool show_hand_aruco = false;
-    bool show_hand_debug = false;
-    bool show_point_cloud = false;
 
     if (std::string(argv[1]) == "true")
         blocking = true;
@@ -48,15 +185,6 @@ int main(int argc, char** argv)
         use_analogs = true;
     if (std::string(argv[6]) == "true")
         show_hand_fk = true;
-    if (std::string(argv[7]) == "true")
-        show_hand_aruco = true;
-    if (std::string(argv[8]) == "true")
-        show_hand_debug = true;
-    if (std::string(argv[9]) == "true")
-        show_point_cloud = true;
-    std::string camera_name = "";
-    if (show_point_cloud)
-        camera_name = std::string(argv[10]);
 
     double fps = 30.0;
     VtkContainer container(1.0 / fps, 600, 600, blocking);
@@ -71,18 +199,32 @@ int main(int argc, char** argv)
         container.add_content("hand_fk", std::move(hand));
     }
 
-    /* Show additional hand for debugging purposes. */
-    // if (show_hand_debug)
-    // {
-    //     std::unique_ptr<VtkContent> hand_debug = std::unique_ptr<VtkiCubHand>
-    //     (
-    //         new VtkiCubHand(robot_name, hand_laterality, "test-visualization/hand_debug", use_fingers, use_analogs, {60.0 / 255.0, 180.0 / 255.0, 60.0 / 255.0}, 1.0)
-    //     );
+    yarp::sig::FlexImage yarpImage;
+    yarp::os::BufferedPort<yarp::sig::FlexImage> outputPort;
+    if (!outputPort.open("/hand-visualizer/" + hand_laterality + "/image"))
+    {
+        return EXIT_FAILURE;
+    }
 
-    //     container.add_content("hand_aruco_debug", std::move(hand_debug));
-    // }
+    container.initialize();
+    container.setOrientationWidgetEnabled(false);
+    container.render_window()->SetAlphaBitPlanes(1);
+    container.renderer()->SetBackground(0, 0, 0);
+    while(!closing)
+    {
+        container.update();
+        vtkNew<vtkWindowToImageFilter> w2if ;
+        w2if->SetInputBufferTypeToRGBA();
+        w2if->SetInput(container.render_window());
+        w2if->Update();
+        vtkImageDataToYarpimage(w2if->GetOutput(), yarpImage);
 
-    container.run();
+        yarp::sig::FlexImage& imageToBeSent = outputPort.prepare();
+        imageToBeSent.setPixelCode(yarpImage.getPixelCode());
+        imageToBeSent.setExternal(yarpImage.getRawImage(), yarpImage.width(), yarpImage.height()); //Avoid to copy
+        outputPort.write();
+
+    }
 
     return EXIT_SUCCESS;
 }
